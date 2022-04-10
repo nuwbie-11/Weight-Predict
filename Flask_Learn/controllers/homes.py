95ae16384668f435dc9a247b6d6a55722ad28e31
Roxasha <63080447+nuwbie-11@users.noreply.github.com>
2022-04-10 16:03:24 +0700

    Fix

--- Flask_Learn/controllers/homes.py
+++ Flask_Learn/controllers/homes.py
@@ -14,10 +14,12 @@ def home():
     elif request.method == 'POST':
         feats = dict(request.form).values()
         feats = np.array([float(x) for x in feats])
-
+        # return f"{feats}"
         model = joblib.load("model/Weight-prediction-using-Linear-regression.pkl")
-
+        gender = "Female" if feats[0] == 0 else "Male"
+        height = feats[1] 
         result = model.predict([feats])
-        return f"{result}"
+        result = f"Around {result[0]:1f}"
+        return render_template('index.html', result=result,gender=gender,height=height)
 
       
